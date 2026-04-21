8050e451694c9078327a91ed7a1690e0f8a01eb3
Jie Li <jl@ocg.de>
2026-04-21 18:20:48 +0200

    added module federation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
+
+import { loadRemoteModule } from "@angular-architects/native-federation";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Module Federation Host';
+
+  loadError = false;
+
+  remoteApp: Type<unknown> | null = null;
+
+  async ngOnInit() {
+    try {
+      const remoteModule = await loadRemoteModule({
+        remoteName: 'remote',
+        exposedModule: './RemoteApp'
+      })
+      this.remoteApp = remoteModule.AppComponent;
+    } catch (err) {
+      console.log('Failed to load remote component', err);
+      this.loadError = true;
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { initFederation } from '@angular-architects/native-federation';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
+initFederation({
+  remote: '/remotes/remote/browser/remoteEntry.json'
 })
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
   .catch(err => console.error(err));
